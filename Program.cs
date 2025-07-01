4ab7df3b1ef13d6cb6863331b06a8299dc91fca9
Faith Sodipe <fsodipe@amscreen.com>
2025-07-01 12:59:56 +0100

    Added Serilog support

--- Program.cs
+++ Program.cs
@@ -1,11 +1,27 @@
-var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+using ProjectR.Backend.Application.AppSettings;
+using Serilog;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Host.UseSerilog((context, services, loggerConfig) =>
+        {
+            loggerConfig
+                .MinimumLevel.Debug()
+                .WriteTo.Console()
+                .WriteTo.File("Logs/applog.txt", rollingInterval: RollingInterval.Day);
+        });
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Configuration
+       .AddJsonFile("appsettings.json", optional: false)
+       .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true);
+
+builder.Services.Configure<TestSettings>(builder.Configuration.GetSection("ProcessingSettings"));
+
 
 var app = builder.Build();
 
@@ -15,6 +31,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseSerilogRequestLogging();
 
 app.UseHttpsRedirection();
 
---
+++ ProjectR.Backend.Application/AppSettings/TestSettings.cs
@@ -0,0 +1,8 @@
+﻿
+namespace ProjectR.Backend.Application.AppSettings
+{
+    public class TestSettings
+    {
+        public string QueueName { get; set; } = string.Empty;
+    }
+}
