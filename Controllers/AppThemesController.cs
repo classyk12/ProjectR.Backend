67c19298087698599f39f51bab207ffa4ec65f0b
Faith Sodipe <fsodipe@amscreen.com>
2025-07-15 16:07:36 +0100

    AppTheme controller actions...WIP

--- Controllers/AppThemesController.cs
+++ Controllers/AppThemesController.cs
@@ -4,6 +4,7 @@ using ProjectR.Backend.Application.Models;
 
 namespace ProjectR.Backend.Controllers
 {
+    [Route("api/[controller]")]
     public class AppThemesController : Controller
     {
         private readonly IAppThemeManager _appThemeManager;
@@ -16,22 +17,39 @@ namespace ProjectR.Backend.Controllers
         [HttpGet]
         public async Task<IActionResult> GetAll()
         {
-            AppThemeModel[] appThemes = await _appThemeManager.GetAllAsync();
-            return Ok(appThemes);
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> Add()
-        {
-            AppThemeModel[] appThemes = await _appThemeManager.GetAllAsync();
-            return Ok(appThemes);
+            AppThemeModel[] result = await _appThemeManager.GetAllAsync();
+            return Ok(result);
         }
 
         [HttpGet("{id}")]
-        public async Task<IActionResult> Add(Guid id)
+        public async Task<IActionResult> Get(Guid id)
         {
-            AppThemeModel appThemes = await _appThemeManager.GetByIdAsync(id: id);
-            return Ok(appThemes);
+            ResponseModel<AppThemeModel> result = await _appThemeManager.GetByIdAsync(id: id);
+            return result.Status ? Ok(result) : NotFound(result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add([FromBody] AppThemeModel appTheme)
+        {
+            if (appTheme == null || !ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            ResponseModel<AppThemeModel> result = await _appThemeManager.AddAsync(appTheme);
+            return result.Status ? Ok(result) : NotFound(result);
+        }
+
+        [HttpPost("AddMany")]
+        public async Task<IActionResult> AddMany([FromBody] AppThemeModel[] appThemes)
+        {
+            if (appThemes.Length == 0 || !ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            ResponseModel<AppThemeModel[]> result = await _appThemeManager.AddAsync(appThemes);
+            return result.Status ? Ok(result) : NotFound(result);
         }
     }
 }
--- ProjectR.Backend.Application/Models/AppThemeModel.cs
+++ ProjectR.Backend.Application/Models/AppThemeModel.cs
@@ -1,7 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace ProjectR.Backend.Application.Models
 {
     public class AppThemeModel
     {
         public Guid Id { get; set; }
+        
+        [Required(ErrorMessage = "Name is required")]
+        public string? Name { get; set; }
     }
 }
