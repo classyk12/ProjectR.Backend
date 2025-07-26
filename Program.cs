914461d97d7b4ef7936a764ba85e0ea5b5aa0556
Classyk <40452639+classyk12@users.noreply.github.com>
2025-07-26 17:59:03 +0100

    Update Program.cs to make appsettings file optional

--- Program.cs
+++ Program.cs
@@ -30,7 +30,7 @@ namespace ProjectR.Backend
 
             builder.Configuration
             .SetBasePath(Directory.GetCurrentDirectory())
-            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
             .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true)
             .AddEnvironmentVariables();
 
